85d3e949e0f330cb16323d469c4f8c0d7541f46d
Alison Keen <ali.keen@gmail.com>
2016-09-15 05:19:50 +0000

    Copied over a starting point for scraper and README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
+
+Scrapes just the list of MPS and their URLS, but not which house they belong to.
+
+BROKEN I can't figure out why this doesn't actually put anything into a Sqlite database!? It does on my own machine but not on Morph.io
+
+Those two csv files need to be generated separately to get their own URLs, so that forked openaustralia parsers can pull them into a database. 
+
+scraper #1: List of MPS - this one:
+fields {id, saparl ID, full name}
+destination: /var/www/openaustralia/openaustralia-parser/data/people.csv
+
+scraper #2: List of MHRs - (coming soon) 
+fields {id, id2, Name, Electorate, State/Territory(?), Start date, Election type, End date, reason, Most recent party}
+
+scraper #3: List of Senators - (coming soon)
+fields {id, id2, Name, Electorate, State/Territory(?), Start date, Election type, End date, reason, Most recent party}
+
+
 This is a scraper that runs on [Morph](https://morph.io). To get started [see the documentation](https://morph.io/documentation)
--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,110 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+#!/bin/env ruby
+# encoding: utf-8
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+# This script started off as a shamelessly plagiarized version of the Popolo/ EveryPolitician Virgin Islands example scraper (thanks, guys!!)
+# Hopefully it isn't too similar but it gave me a very readable starting point, as a ruby beginner. :) 
+# - Alison
+
+# Original is here: 
+# https://github.com/tmtmtmtm/us-virgin-islands-legislature/blob/master/scraper.rb
+
+require 'scraperwiki'
+require 'nokogiri'
+require 'open-uri'
+
+# This is stopping me from debugging/running
+# what do I need these for? 
+
+# require 'colorize'
+# require 'pry'
+# require 'open-uri/cached'
+# OpenURI::Cache.cache_path = '.cache'
+
+sa_mps_url = 'https://www2.parliament.sa.gov.au/Internet/DesktopModules/Memberlist.aspx'
+
+def noko_for(url)
+  Nokogiri::HTML(open(url).read)
+end
+
+def scrape_list(url)
+  noko = noko_for(url)
+  noko.css('table table table tr').css('a[href*="MemberDrill"]').each do |a|
+    mp_url = URI.join url, a.attr('href')
+    scrape_person(a.text, mp_url)
+  end
+end
+
+def scrape_person(mp_name, url)
+
+   noko = noko_for(url)
+
+   #reset vars for each MP
+   electorate_name = nil, which_party = nil, which_house = nil
+   email_address = nil
+   
+   puts "\nName: " + mp_name.to_s
+
+   #Image is located in a different place to other info
+   image_tag = noko.css('img[src*="PersonImage"]')[0]
+   image_src = URI.join url, image_tag.attr('src')
+   puts "Image: " + image_src.to_s
+
+   # Iterate over the rows of general info and try and
+   # identify specific information
+   info_rows = noko.css('table table table tr')
+   info_rows.each do |row|
+
+     #Reset variables for each row
+     label = nil, cell_content = nil
+     info_cells = row.css('td[class*="boxedtext"]')
+     next if info_cells[0] == nil  # ignore lines with no "boxedtext"
+
+     label = info_cells[0].text.strip 
+     cell_content = info_cells[1].text.strip 
+
+     # Check if line contains Electorate
+     if label.to_s == "Electorate" 
+        electorate_name = cell_content
+	puts "Electorate: " + electorate_name
+     end
+ 
+     # Check if it's which house
+     if label.to_s == "House" 
+        which_house = cell_content
+	puts "House: " + which_house
+     end
+
+     # Check if line contains MP email
+     if label.to_s == "Email" 
+        email_address = cell_content
+	puts "Email: " + email_address
+     end
+ 
+     # Check if line contains MP email
+     if label.to_s == "Political Party" 
+        which_party = cell_content
+	puts "Party: " + which_party
+     end
+ 
+   end #end row interation
+
+   data = { 
+     id: url.to_s.split("=").last,
+     id__saparl: url.to_s.split("=").last,
+     full_name: mp_name,
+     house: which_house,
+     email: email_address,
+     image: image_src,
+     profile_page: url.to_s,
+   }
+
+   data[:image] = URI.join(url, data[:image]).to_s unless data[:image].to_s.empty?
+
+   ScraperWiki.save_sqlite([:id], data)
+end
+
+scrape_list(sa_mps_url)
+
+#Testing: just try it on one page first... 
+#scrape_person('Frances Bedford', 'https://www2.parliament.sa.gov.au/Internet/DesktopModules/MemberDrill.aspx?pid=543')
